69ce658ad15fd09bd6ef5cccf1eecfa3b9dede1a
saiderrahmani <didi.mousedidi@outlook.com>
2023-07-09 22:39:43 -0400

    update lib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_sequence = []
+    x, y = 0, 1
+
+    for _ in range(length):
+        fibonacci_sequence.append(x)
+        x, y = y, x + y
+
+    print(fibonacci_sequence)
